698934a84555b7ef0d7cbaeda4ed6a7cf0e9d8d0
Ben Cail <bcail@crossway.org>
2023-11-10 16:05:48 -0500

    more work on setting things up

--- webrtc_server.py
+++ webrtc_server.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# Resources
+#   https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Signaling_and_video_calling
+#   https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createAnswer
+#   https://codelabs.developers.google.com/codelabs/webrtc-web/#0
+#   https://github.com/googlecodelabs/webrtc-web/tree/master (Apache-2.0 License)
+
 from http.server import HTTPServer, HTTPStatus, BaseHTTPRequestHandler
 import json
 import random
@@ -18,36 +26,47 @@ BASE_HTML = '''
 <body>
   <h1>Test WEBRTC</h1>
   <div id="content">
+    <div>
+      <p>Local Video</p>
+      <video id="localVideo" autoplay playsinline />
+    </div>
+    <div>
+      <p>Remote Video</p>
+      <video id="remoteVideo" autoplay playsinline />
+    </div>
 
-%s
-
+    <div>
+      <button id="startButton">Start Local Stream</button>
+      <button id="connectButton">Connect</button>
+      <button id="getRemoteButton">Get Remote Stream</button>
+      <button id="hangupButton">Hang Up</button>
+    </div>
   </div>
-</body>
-</html>
-'''
-
-CONNECT_HTML = BASE_HTML % '''
-  <div>
-    <p>Local Video</p>
-    <video id="localVideo" autoplay playsinline />
-  </div>
-  <div>
-    <p>Remote Video</p>
-    <video id="remoteVideo" autoplay playsinline />
-  </div>
-
-  <div>
-    <button id="startButton">Start Local Stream</button>
-    <button id="connectButton">Connect</button>
-    <button id="hangupButton">Hang Up</button>
-  </div>
-
-  <div id="localOffer">Local offer description</div>
-  <div id="remoteOffer">Remote offer description</div>
 
 <script type="text/javascript">
-// https://github.com/googlecodelabs/webrtc-web/tree/master (Apache-2.0 License)
 'use strict';
+
+// Define helper functions.
+
+// Gets the "other" peer connection.
+function getOtherPeer(peerConnection) {
+  return (peerConnection === localPeerConnection) ?
+      remotePeerConnection : localPeerConnection;
+}
+
+// Gets the name of a certain peer connection.
+function getPeerName(peerConnection) {
+  return (peerConnection === localPeerConnection) ?
+      'localPeerConnection' : 'remotePeerConnection';
+}
+
+// Logs an action (text) and the time when it happened on the console.
+function trace(text) {
+  text = text.trim();
+  const now = (window.performance.now() / 1000).toFixed(3);
+
+  console.log(now, text);
+}
 
 // In this codelab, you will be streaming video only: "video: true".
 // Audio will not be streamed because it is set to "audio: false" by default.
@@ -62,9 +81,6 @@ const offerOptions = {
 
 // Define initial start time of the call (defined as connection between peers).
 let startTime = null;
-
-// Define Client ID (different for different page loads)
-let clientId = %s;
 
 // Define peer connections, streams and video elements.
 const localVideo = document.getElementById('localVideo');
@@ -82,8 +98,8 @@ let remotePeerConnection;
 function gotLocalMediaStream(mediaStream) {
   localVideo.srcObject = mediaStream;
   localStream = mediaStream;
-  trace('Received local stream.');
-  connectButton.disabled = false;  // Enable call button.
+  // trace('Received local stream.');
+  connectButton.disabled = false;
 }
 
 // Handles error by logging a message to the console.
@@ -187,42 +203,70 @@ function setRemoteDescriptionSuccess(peerConnection) {
   setDescriptionSuccess(peerConnection, 'setRemoteDescription');
 }
 
+// Define and add behavior to buttons.
+
+// Define action buttons.
+const startButton = document.getElementById('startButton');
+const connectButton = document.getElementById('connectButton');
+const getRemoteButton = document.getElementById('getRemoteButton');
+const hangupButton = document.getElementById('hangupButton');
+
+// Set up initial action buttons status: disable call and hangup.
+connectButton.disabled = true;
+hangupButton.disabled = true;
+
+// Handles start button action: creates local MediaStream.
+function startAction() {
+  startButton.disabled = true;
+  navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+  // trace('Requesting local stream.');
+}
+
+// Handles hangup action: ends up call, closes connections and resets peers.
+function hangupAction() {
+  localPeerConnection.close();
+  remotePeerConnection.close();
+  localPeerConnection = null;
+  remotePeerConnection = null;
+  hangupButton.disabled = true;
+  connectButton.disabled = false;
+  trace('Ending call.');
+}
+
+%s
+
+</script>
+</body>
+</html>
+'''
+
+CONNECT_HTML = BASE_HTML % '''
+// Define Client ID (different for different page loads)
+let clientId = %s;
+
 // Logs offer creation and sets local peer connection session descriptions;
 // posts offer data to server
 function createdOffer(description) {
-  trace(`Offer from localPeerConnection:\n${description.sdp}`);
-
-  trace('localPeerConnection setLocalDescription start.');
+  // trace('localPeerConnection setLocalDescription start.');
   localPeerConnection.setLocalDescription(description)
     .then(() => {
-      setLocalDescriptionSuccess(localPeerConnection);
+      // setLocalDescriptionSuccess(localPeerConnection);
     }).catch(setSessionDescriptionError);
 
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "/", true);
+  xhr.open("POST", "/meet", true);
   // Send the proper header information along with the request
   xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
   xhr.onreadystatechange = () => {
     // Call a function when the state changes.
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       // Request finished. Do processing here.
+      console.log("request finished");
     }
   };
   xhr.send(JSON.stringify({"id": clientId, "offer": description}));
-  // xhr.send(new Int8Array());
-  // xhr.send(document);
 
-
-  // trace('remotePeerConnection setRemoteDescription start.');
-  // remotePeerConnection.setRemoteDescription(description)
-  //   .then(() => {
-  //     setRemoteDescriptionSuccess(remotePeerConnection);
-  //   }).catch(setSessionDescriptionError);
-
-  // trace('remotePeerConnection createAnswer start.');
-  // remotePeerConnection.createAnswer()
-  //   .then(createdAnswer)
-  //   .catch(setSessionDescriptionError);
 }
 
 // Logs answer to offer creation and sets peer connection session descriptions.
@@ -242,25 +286,6 @@ function createdAnswer(description) {
     }).catch(setSessionDescriptionError);
 }
 
-// Define and add behavior to buttons.
-
-// Define action buttons.
-const startButton = document.getElementById('startButton');
-const connectButton = document.getElementById('connectButton');
-const hangupButton = document.getElementById('hangupButton');
-
-// Set up initial action buttons status: disable call and hangup.
-connectButton.disabled = true;
-hangupButton.disabled = true;
-
-// Handles start button action: creates local MediaStream.
-function startAction() {
-  startButton.disabled = true;
-  navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
-  trace('Requesting local stream.');
-}
-
 // Handles call button action: creates peer connection.
 function connectAction() {
   connectButton.disabled = true;
@@ -273,24 +298,24 @@ function connectAction() {
   const videoTracks = localStream.getVideoTracks();
   const audioTracks = localStream.getAudioTracks();
   if (videoTracks.length > 0) {
-    trace(`Using video device: ${videoTracks[0].label}.`);
+    // trace(`Using video device: ${videoTracks[0].label}.`);
   }
   if (audioTracks.length > 0) {
-    trace(`Using audio device: ${audioTracks[0].label}.`);
+    // trace(`Using audio device: ${audioTracks[0].label}.`);
   }
 
   const servers = null;  // Allows for RTC server configuration.
 
   // Create peer connections and add behavior.
   localPeerConnection = new RTCPeerConnection(servers);
-  trace('Created local peer connection object localPeerConnection.');
+  // trace('Created local peer connection object localPeerConnection.');
 
   localPeerConnection.addEventListener('icecandidate', handleConnection);
   localPeerConnection.addEventListener(
     'iceconnectionstatechange', handleConnectionChange);
 
   remotePeerConnection = new RTCPeerConnection(servers);
-  trace('Created remote peer connection object remotePeerConnection.');
+  // trace('Created remote peer connection object remotePeerConnection.');
 
   remotePeerConnection.addEventListener('icecandidate', handleConnection);
   remotePeerConnection.addEventListener(
@@ -299,52 +324,43 @@ function connectAction() {
 
   // Add local stream to connection and create offer to connect.
   localPeerConnection.addStream(localStream);
-  trace('Added local stream to localPeerConnection.');
+  // trace('Added local stream to localPeerConnection.');
 
-  trace('localPeerConnection createOffer start.');
+  // trace('localPeerConnection createOffer start.');
   localPeerConnection.createOffer(offerOptions)
     .then(createdOffer).catch(setSessionDescriptionError);
 }
 
-// Handles hangup action: ends up call, closes connections and resets peers.
-function hangupAction() {
-  localPeerConnection.close();
-  remotePeerConnection.close();
-  localPeerConnection = null;
-  remotePeerConnection = null;
-  hangupButton.disabled = true;
-  connectButton.disabled = false;
-  trace('Ending call.');
+// Handles call button action: creates peer connection.
+function getRemoteAction() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/meet/" + clientId, true);
+  // Send the proper header information along with the request
+  xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  xhr.onreadystatechange = () => {
+    // Call a function when the state changes.
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Request finished. Do processing here.
+      console.log("request finished: " + xhr.responseText);
+      trace('remotePeerConnection setRemoteDescription start.');
+      remotePeerConnection.setRemoteDescription(JSON.parse(xhr.responseText))
+        .then(() => {
+          setRemoteDescriptionSuccess(remotePeerConnection);
+        }).catch(setSessionDescriptionError);
+      trace('remotePeerConnection createAnswer start.');
+      remotePeerConnection.createAnswer()
+        .then(createdAnswer)
+        .catch(setSessionDescriptionError);
+    }
+  };
+  xhr.send();
 }
 
 // Add click event handlers for buttons.
 startButton.addEventListener('click', startAction);
 connectButton.addEventListener('click', connectAction);
+getRemoteButton.addEventListener('click', getRemoteAction);
 hangupButton.addEventListener('click', hangupAction);
-
-// Define helper functions.
-
-// Gets the "other" peer connection.
-function getOtherPeer(peerConnection) {
-  return (peerConnection === localPeerConnection) ?
-      remotePeerConnection : localPeerConnection;
-}
-
-// Gets the name of a certain peer connection.
-function getPeerName(peerConnection) {
-  return (peerConnection === localPeerConnection) ?
-      'localPeerConnection' : 'remotePeerConnection';
-}
-
-// Logs an action (text) and the time when it happened on the console.
-function trace(text) {
-  text = text.trim();
-  const now = (window.performance.now() / 1000).toFixed(3);
-
-  console.log(now, text);
-}
-
-</script>
 '''
 
 
@@ -352,38 +368,79 @@ offers = {0: {}}
 
 
 def render_template():
+    client_id = 0
     for i in range(1, 3):
         if i not in offers:
             offers[i] = {}
             client_id = i
             break
-    if not client_id:
-        client_id = 0
     return CONNECT_HTML % client_id
+
+
+MEETING_PATH = '/meet'
 
 
 class Handler(BaseHTTPRequestHandler):
     def version_string(self):
         return 'Apache'
 
-    def log_request(self, with_headers=False):
-        self.log_message('"%s"', self.requestline)
+    def log_request(self, with_headers=False, not_found=False):
+        msg = '"%s"'
+        if not_found:
+            msg = '"%s" => 404 Not Found'
+        self.log_message(msg, self.requestline)
         if with_headers:
             self.log_message('  headers:')
             for header, value in self.headers.items():
                 self.log_message('    %s: %s', header, value)
 
+    def not_found(self):
+        self.log_request(not_found=True)
+        self.send_response_only(HTTPStatus.NOT_FOUND)
+        self.end_headers()
+
     def do_GET(self):
+        if not self.path.startswith(MEETING_PATH):
+            self.not_found()
+            return
+
         self.log_request()
+
+        content_type = 'text/html'
+
+        if self.path.endswith('/1'):
+            if 2 in offers and 'offer' in offers[2]:
+                content = json.dumps(offers[2]['offer'])
+                content_type = 'application/json'
+            else:
+                content = 'no offer yet'
+                content_type = 'text/plain'
+        elif self.path.endswith('/2'):
+            if 1 in offers and 'offer' in offers[1]:
+                content = json.dumps(offers[1]['offer'])
+                content_type = 'application/json'
+            else:
+                content = 'no offer yet'
+                content_type = 'text/plain'
+        else:
+            content = render_template()
+
+        encoded_content = content.encode('utf8')
+
         self.send_response_only(HTTPStatus.OK)
         self.send_header('Server', self.version_string())
         self.send_header('Date', self.date_time_string())
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', len(encoded_content))
         self.end_headers()
-
-        self.wfile.write(render_template().encode('utf8'))
+        self.wfile.write(encoded_content)
 
     def do_POST(self):
-        self.log_request(with_headers=True)
+        if not self.path.startswith(MEETING_PATH):
+            self.not_found()
+            return
+
+        self.log_request(with_headers=False)
 
         content_length = self.headers.get('Content-Length')
         try:
@@ -397,15 +454,18 @@ class Handler(BaseHTTPRequestHandler):
             body = body.decode('utf8')
             if content_type.startswith('application/json'):
                 data = json.loads(body)
-                offers[data['id']] = data['offer']
-                print(f'{offers=}')
+                offers[data['id']]['offer'] = data['offer']
+                print(offers.keys())
             else:
                 print(f'{body=}')
 
         self.send_response_only(HTTPStatus.OK)
         self.send_header('Server', self.version_string())
         self.send_header('Date', self.date_time_string())
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
+
+        self.wfile.write('finished POST handling'.encode('utf8'))
 
 
 def main(port=8000):
